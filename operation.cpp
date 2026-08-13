624ddb52a81fe82cfacd370e2e55172bf8379429
srushti-1507 <srushtimenthe@gmail.com>
2026-08-13 12:51:43 +0000

    operation

---
+++ operation.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,sum, sub,multi,div;
+    cout<<"Enter number a : ";
+    cin>>a;
+    cout<<"Enter number b :";
+    cin>>b;
+    sum=a+b;
+    sub=a-b;
+    multi=a*b;
+    div=a/b;
+    cout<<"the addition is:"<<sum <<endl;
+    cout<<"the substraction is :"<<sub <<endl;
+    cout<<"the multiplication is : "<<multi <<endl;
+    cout<<"the division is :"<<div<<endl;
+
+    return 0;
+    
+}
