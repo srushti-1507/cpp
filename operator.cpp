0e444695da73cb65b65307f40f32c7c29ce95791
srushti-1507 <srushtimenthe@gmail.com>
2026-08-20 12:41:07 +0000

    operator

---
+++ operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int  main(){
+    int a,b,add,sub,multi,div;
+    a = 5;
+    b = 4;
+    cout<<"+"<<(a+b)<<endl;
+    cout<<"-"<<(a-b)<<endl;
+    cout<<"*"<<(a*b)<<endl;
+    cout<<"/"<<(a/b)<<endl;
+    cout<<"%"<<(a%b)<<endl;
+    return 0;
+
+}
