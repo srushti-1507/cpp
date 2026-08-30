86c7c7581f24b78de81ba88b4efc89282029108a
srushti Menthe <srushtimenthe07@gamil.com>
2026-08-30 14:00:01 +0530

    Added challenge1

---
+++ challenge1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int myNum = 10;
+    cout << myNum <<endl;
+    
+    myNum += 5;
+    cout << myNum <<endl;
+    
+     int myNum2 = myNum*2*2*2;
+     cout << myNum2 << endl;
+
+    return 0;
+}
