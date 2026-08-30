944438f7cb544f9c6f96c0cb02db610eda1e4680
srushti Menthe <srushtimenthe07@gamil.com>
2026-08-30 16:43:43 +0530

    if+else if

--- p3_2.cpp
+++ p3_2.cpp
@@ -1,5 +1,24 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int age;
+
+    cout << "Welcome to the Pub and Grille! "<< endl;
+    cout << " Please enter your age :" << endl;
+    cin >> age;
+
+    if(age >= 21 ){
+        cout << "Would you like a beer?" <<endl;
+
+    }
+    else if(age >= 16 ){
+    cout << "would you like a coke ? "<< endl;
+    cout << "At least you can drive !" << endl;
+}
+else{    cout << "Would You like coke ?" << endl;
+}
+cout<< "thanks for coming to the Pub and Grille!"<< endl;
+
+
+   return 0;
 }
